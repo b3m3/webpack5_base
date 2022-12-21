ed6f972a49f574a043782bb4820c3877f8c620cd
b3m3 <uwm.d.c.a@gmail.com>
2022-12-21 22:52:50 +0200

    change path script/style

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src/js/', 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.[contenthash].js',
